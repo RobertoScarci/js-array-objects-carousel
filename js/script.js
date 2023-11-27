24a687ca48629812e5b2de1cb4b357e71b75da58
RobertoScarci <robertoscarci.02@gmail.com>
2023-11-27 18:41:29 +0100

    costruzione dello script e delle funzioni interne

--- js/script.js
+++ js/script.js
@@ -21,3 +21,47 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+const listElement = document.querySelector('div.scroll');
+
+const beforeButton = document.querySelector('.prev');
+const afterButton = document.querySelector('.next');
+
+let activeImage = 0;
+
+
+createImage(images, listElement, activeImage);
+
+beforeButton.addEventListener("click", () => {
+
+    activeImage == 0 ? activeImage = images.length : 0;
+
+    activeImage -= 1;
+
+    createImage(images, listElement, activeImage);
+});
+
+afterButton.addEventListener("click", () => {
+
+    activeImage == images.length - 1 ? activeImage = -1 : 0;
+
+    activeImage += 1;
+
+    createImage(images, listElement, activeImage);
+});
+
+
+// ==== Functions ====
+
+function createImage(image, output, activeImage) {
+    output.innerHTML += `
+    <div>
+        <img src="${image[activeImage].image}" alt="${image[activeImage].title} game">
+        <div class="info">
+            <h2>${image[activeImage].title}</h2>
+            <p>${image[activeImage].text}</p>
+        </div>
+    </div>`
+}
+
+
